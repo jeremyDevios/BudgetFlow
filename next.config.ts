5e2b1d9c9a8c75cd90caef68af636c4b344ddd1b
jeremyDevios <jeremy.Devios@gmail.com>
2026-01-20 14:58:41 +0100

    Change config server

---
+++ next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  // Optionnel: Permettre l'accès depuis des domaines spécifiques en mode dev (si bloqué)
+  // experimental: {
+  //   allowedDevOrigins: ["budget.zikkis.fr"],
+  // },
+};
+
+export default nextConfig;
